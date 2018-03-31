5d2785341720e00379408c1c16447c317ef83a46
Nick Mertens <nickemans@gmail.com>
2018-04-01 11:46:19 +1200

    Fix syntax

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 # Copy everything to build folder
 cp favicon.ico build
 cp -R css js images build
 
 # Deploy build folder
 cd build
-find . -type f -exec curl --user $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://quoralis.com/curltest/{} \;
+find . -type f -exec curl --user ${FTP_USER}:${FTP_PASSWORD} --ftp-create-dirs -T {} ftp://quoralis.com/curltest/{} \;
