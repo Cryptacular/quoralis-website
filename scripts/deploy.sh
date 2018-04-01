1547e7690c17b751a358e80372a9bd6bd6906e62
Nick Mertens <nickemans@gmail.com>
2018-04-01 12:14:31 +1200

    Update to deploy on master branch, and to the production folder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ cp -R css js images build
 
 # Deploy build folder
 cd build
-find . -type f -exec curl --user ${FTP_USER}:${FTP_PASSWORD} --ftp-create-dirs -T {} ftp://quoralis.com/curltest/{} \;
+find . -type f -exec curl --user ${FTP_USER}:${FTP_PASSWORD} --ftp-create-dirs -T {} ftp://quoralis.com/httpdocs/{} \;
